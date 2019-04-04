1a5a3e21a6ad2365e9a3cd621958ad4d8a11a9d6
Michael Scott <mscott41@gmail.com>
2019-04-03 21:07:05 -0400

    remove instructions and pseudo code

--- guessing_game.py
+++ guessing_game.py
@@ -1,14 +1,3 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces.
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
 number = random.randint(1, 10)
 
@@ -18,22 +7,7 @@ def start_game():
     guess_remaining = [1, 2, 3, 4, 5]
     guesses_used = 0
     group = []
-    """Psuedo-code Hints
 
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
     # write your code inside this function.
 
     input("Welcome to the Number Guessing Game!  Press ENTER to start.")
